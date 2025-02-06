9a7fa4e0d5d01ec404692d9087849fdff7e3f546
LearnerSumit <sumit.202104177@tulas.edu.in>
2025-02-06 19:27:58 +0530

    add / point for testing

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/media", require("./routes/mediaRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
